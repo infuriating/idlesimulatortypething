7d62289563455898202ee7c30024731b02255d77
BuildTools <unconfigured@null.spigotmc.org>
2023-02-19 00:50:11 +0100

    asfasdf

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,14 +1,26 @@
 for (let i = 0; i < saveState.upgrades.length; i++) {
   document.getElementById("upgrades").innerHTML += `
         <div class="upgrade">
-        <div class="upgrade-name">${saveState.upgrades[i].name}</div>
-        <div class="upgrade-cost">Cost: ${saveState.upgrades[i].cost}</div>
-        <div class="upgrade-amount">Amount: ${saveState.upgrades[i].amount}</div>
-        <div class="upgrade-description">${saveState.upgrades[i].description}</div>
-        <button onclick="buyUpgrade(${i})">Upgrade</button>
+        <div class="upgrade__name">${saveState.upgrades[i].name}</div>
+        <div class="upgrade__description">${saveState.upgrades[i].description}</div>
+        <div class="upgrade__details">
+        <div class="upgrade__cost">Cost: ${saveState.upgrades[i].cost}</div>
+        <div class="upgrade__amount">Amount: ${saveState.upgrades[i].amount}</div>
+        </div>
+        <button class="upgrade__button" onclick="buyUpgrade(${i})">Upgrade</button>
         </div>
     `;
 }
+
+// setInterval(() => {
+//   if (saveState.upgrades[upgrade].cost > saveState.cash) {
+//     document.getElementsByClassName("upgrade").style.backgroundColor = "#000";
+//     document.getElementsByClassName("upgrade__cost").style.color = "red";
+//     document.getElementsByClassName("upgrade__button").style.opacity = "0.5";
+//     document.getElementsByClassName("upgrade__button").style.cursor =
+//       "not-allowed";
+//   }
+// }, 50);
 
 function buyUpgrade(upgrade) {
   if (saveState.cash >= saveState.upgrades[upgrade].cost) {
@@ -19,10 +31,10 @@ function buyUpgrade(upgrade) {
     saveState.addCash += saveState.upgrades[upgrade].addCash;
     saveState.cashMultiplier += saveState.upgrades[upgrade].multiplier;
     saveState.multiply *= saveState.upgrades[upgrade].multiply;
-    document.getElementsByClassName("upgrade-cost")[
+    document.getElementsByClassName("upgrade__cost")[
       upgrade
     ].innerHTML = `Cost: ${saveState.upgrades[upgrade].cost.toFixed(1)}`;
-    document.getElementsByClassName("upgrade-amount")[
+    document.getElementsByClassName("upgrade__amount")[
       upgrade
     ].innerHTML = `Amount: ${saveState.upgrades[upgrade].amount}`;
   }
