99070b491f620d8775d05b610f435234cb140159
BuildTools <unconfigured@null.spigotmc.org>
2023-02-19 10:08:49 +0100

    lots of changes, A LOT WOOOOOOW

--- js/onLoad.js
+++ js/onLoad.js
@@ -1,11 +1,14 @@
 //loads the data from the local storage
 const saveState = {
   addCash: 1,
-  cash: 2,
+  cash: 0,
+  totalCash: 0,
   cashMultiplier: 0.1,
   multiply: 1,
+  upgradeTotalAmount: 0,
   upgrades: [
     {
+      max: 10,
       name: "Help yourself",
       cost: 10,
       costIncreaser: 1.2,
@@ -16,6 +19,7 @@ const saveState = {
       description: "Add a extra dollar per second",
     },
     {
+      max: 25,
       name: "Faster!",
       cost: 100,
       costIncreaser: 1.2,
@@ -26,6 +30,7 @@ const saveState = {
       description: "Add 0.1$ to the multiplier",
     },
     {
+      max: 10,
       name: "Multiply the Multiplier",
       cost: 1000,
       costIncreaser: 1.5,
@@ -49,8 +54,10 @@ function loadData() {
   if (loadState !== null) {
     saveState.addCash = loadState.addCash;
     saveState.cash = loadState.cash;
+    saveState.totalCash = loadState.totalCash;
     saveState.cashMultiplier = loadState.cashMultiplier;
     saveState.multiply = loadState.multiply;
     saveState.upgrades = loadState.upgrades;
+    saveState.upgradeTotalAmount = loadState.upgradeTotalAmount;
   }
 }
